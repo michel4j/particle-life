c3c4adeb4fe9b64f84bd5fb379dd66cda8bea9d0
Lennart Mol <lc.mol@student.avans.nl>
2023-05-06 12:27:37 +0200

    Basic movement with update loop

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from tkinter import Tk, Frame, Canvas
 import random
 
 canvasSize = 800
+dt = 0.02
+particlesToDraw = []
+particles = []
 
 
 def genRandomParticles(numParticles, color):
@@ -15,29 +18,48 @@ def genRandomParticles(numParticles, color):
 
     return particles
 
-def main():
-    window=Tk()
+def update():
+
+
+
+
+    # move particles
+    object = 1
+    for prtcl in particlesToDraw:
+        # change to new value
+        prtcl.posX += 1
+        prtcl.posY += 1
+        canvas.moveto(object, prtcl.posX, prtcl.posY)
+        object += 1
     
+    
+
+def main():
+    global window
+    window=Tk()
+    window.title('Particle Life')
+    global canvas
     canvas = Canvas(width=canvasSize, height=canvasSize, background='black')
     canvas.pack()
 
-    particles = []
+    # whiteParticles = genRandomParticles(25, "white")
+    # redParticles = genRandomParticles(25, "red")
+    # blueParticles = genRandomParticles(25, "blue")
 
-    whiteParticles = genRandomParticles(25, "white")
-    redParticles = genRandomParticles(25, "red")
-    blueParticles = genRandomParticles(25, "blue")
+    # particles.extend(whiteParticles)
+    # particles.extend(redParticles)
+    # particles.extend(blueParticles) 
 
-    particles.extend(whiteParticles)
-    particles.extend(redParticles)
-    particles.extend(blueParticles) 
+    particlesToDraw.append(particle.Particle(400, 400, "blue"))
+    particlesToDraw.append(particle.Particle(600, 600, "red"))
 
-    for prtcl in particles:
-        canvas.create_oval(prtcl.posX, prtcl.posY, prtcl.posX + prtcl.size, prtcl.posY + prtcl.size, fill=prtcl.color)
+    for prtcl in particlesToDraw:
+        particles.append(canvas.create_oval(prtcl.posX, prtcl.posY, prtcl.posX + prtcl.size, prtcl.posY + prtcl.size, fill=prtcl.color))
 
+    while True:
+        update()
+        window.update_idletasks()
+        window.update()
     
-    window.title('Particle Life')
-    window.mainloop()
-    
-
 if __name__ == '__main__':
     main()
--- particle.py
+++ particle.py
@@ -1,7 +1,10 @@
 class Particle:
-    size = 10
+    size = 5
 
     def __init__(self, posX, posY, color):
+        self.color = color
         self.posX = posX
         self.posY = posY
-        self.color = color
+        self.velX = 0
+        self.velY = 0
+        
