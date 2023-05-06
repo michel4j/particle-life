62ef59fff1e2f048ee266a873846b2c0810b8b26
Lennart Mol <lc.mol@student.avans.nl>
2023-05-06 17:06:30 +0200

    More matrices

--- main.py
+++ main.py
@@ -10,10 +10,25 @@ class Color(enum.Enum):
    blue = 1
    green = 2
 
-matrix =[[0.3,   -0.5,   -0.3, 0.3 ], 
-         [1,     0.5,    1,    0.5 ],
-         [-0.5,  1,      0.5,  -1  ],
-         [-0.3,  1,      0.5,  0.8 ]]
+matrix1 =[[1,    0.8,  0.6,  0.4    ], 
+          [0.8,  1,    0.8,  0.6    ],
+          [0.6,  0.8,  1,    0.8    ],
+          [0.4,  0.6,  0.8,  1      ]]
+
+matrix2 =[[0.3,   -0.5,   -0.3, 0.3  ], 
+          [1,     0.5,    1,    0.5  ],
+          [-0.5,  1,      0.5,  -1   ],
+          [-0.3,  1,      0.5,  0.8  ]]
+
+matrix3 =[[-1,    0.8,  0.6,  0.4    ], 
+          [0.8,  -1,    0.8,  0.6    ],
+          [0.6,  0.8,  -1,    0.8    ],
+          [0.4,  0.6,  0.8,  -1      ]]
+
+matrix3 =[[-1,    0.8,  0.6,  0.4    ], 
+          [0.8,  -1,    -0.8,  0.6    ],
+          [-0.6,  0.8,  -1,    0.8    ],
+          [0.4,  -0.6,  0.8,  -1      ]]
 
 def generateRandomAccelerationMatrix():
     matrix = [[0 for x in range(len(Color)+1)] for y in range(len(Color)+1)]
@@ -23,14 +38,15 @@ def generateRandomAccelerationMatrix():
     return matrix
 
 #matrix = generateRandomAccelerationMatrix()
+matrix = matrix3
 
 canvasSizeXMin = 0
-canvasSizeXMax = 1200
+canvasSizeXMax = 800
 canvasSizeYMin = 0
-canvasSizeYMax = 1200
+canvasSizeYMax = 800
 
 dt = 0.01
-rMax = 300
+rMax = 150
 particles = []
 frictionHalfLife = 0.04
 frictionFactor = math.pow(0.5, dt / frictionHalfLife)
