b4d72f7d722aed27b10d47057b8931abd6e790a6
Lennart Mol <lc.mol@student.avans.nl>
2023-05-06 11:36:31 +0200

    Generate random particles

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import particle
 from tkinter import Tk, Frame, Canvas
+import random
+
+canvasSize = 800
+
+
+def genRandomParticles(numParticles, color):
+    particles = []
+
+    for i in range(numParticles):
+        posX = random.uniform(0, canvasSize)
+        posY = random.uniform(0, canvasSize)
+        particles.append(particle.Particle(posX, posY, color))
+
+    return particles
 
 def main():
     window=Tk()
     
-    canvas = Canvas(width=800, height=800, background='black')
+    canvas = Canvas(width=canvasSize, height=canvasSize, background='black')
     canvas.pack()
 
     particles = []
 
-    particles.append(particle.Particle(50, 50, "red"))
-    particles.append(particle.Particle(20, 20, "blue"))
-    particles.append(particle.Particle(100, 200, "yellow"))
-    particles.append(particle.Particle(700, 700, "green"))
-    particles.append(particle.Particle(500, 500, "purple"))
+    whiteParticles = genRandomParticles(25, "white")
+    redParticles = genRandomParticles(25, "red")
+    blueParticles = genRandomParticles(25, "blue")
+
+    particles.extend(whiteParticles)
+    particles.extend(redParticles)
+    particles.extend(blueParticles) 
 
     for prtcl in particles:
         canvas.create_oval(prtcl.posX, prtcl.posY, prtcl.posX + prtcl.size, prtcl.posY + prtcl.size, fill=prtcl.color)
 
+    
     window.title('Particle Life')
-    window.geometry("800x800")
-    window.configure(background='white')
     window.mainloop()
     
 
